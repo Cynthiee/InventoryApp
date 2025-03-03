6e737f836af848381634dc49a7e4670a334661fa
Cynthiee <islacynthia7@gmail.com>
2025-03-04 00:53:34 +0100

    smthing

--- inventory/settings.py
+++ inventory/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-50)_a4$5n)*)vkw30rf2)%kn2mwf1tp^thlxo*z0knr-0up$z4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
