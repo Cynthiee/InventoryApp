b8b0b3199dac81d5e05d0eb1b4985dead5c9d087
Cynthiee <islacynthia7@gmail.com>
2025-03-14 13:01:16 +0100

    fixed

--- inventory/settings.py
+++ inventory/settings.py
@@ -14,12 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ('SECRET_KEY', 'default-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ".render.com,localhost,127.0.0.1").split(",")
 
 
 # Application definition
